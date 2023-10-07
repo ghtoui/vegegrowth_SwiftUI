da3c97485fe08d48049dc52cfee0944e8c58f8dc
ghtoui <92294389+ghtoui@users.noreply.github.com>
2023-10-07 16:54:26 +0900

    UIテストの削除とlint修正

--- vegegrowth_SwiftUITests/vegegrowth_SwiftUITests.swift
+++ vegegrowth_SwiftUITests/vegegrowth_SwiftUITests.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import vegegrowth_SwiftUI
 
-final class vegegrowthSwiftUITests: XCTestCase {
+final class VegegrowthSwiftUITests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
