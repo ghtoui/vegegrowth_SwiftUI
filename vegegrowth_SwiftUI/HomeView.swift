7a97d1cbd351d6035098861435851279d238db9d
ghtoui <92294389+ghtoui@users.noreply.github.com>
2023-10-07 21:55:39 +0900

    ホーム画面を表示できた。UIはまだ

---
+++ vegegrowth_SwiftUI/HomeView.swift
@@ -0,0 +1,41 @@
+//
+//  TableScreenView.swift
+//  vegegrowth_SwiftUI
+//
+//  Created by toui on 2023/10/07.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack(spacing: 30) {
+            Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100, height: 100)
+            Button(action: {}, label: {
+                Text("Swift Button")
+            })
+            Rectangle()
+                .stroke(lineWidth: 4)
+                .foregroundColor(Color.red)
+                .frame(width: 100, height: 100)
+            ZStack(alignment: .bottom) {
+                Circle()
+                    .stroke(lineWidth: 4)
+                    .fill(Color.blue)
+                    .frame(width: 100, height: 100)
+                Text("円")
+            }
+            Spacer()
+        }
+    }
+}
+
+// これがPreview
+struct HomeViewPreviews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
---
+++ vegegrowth_SwiftUI/HomeViewController.swift
@@ -0,0 +1,39 @@
+//
+//  TableViewController.swift
+//  vegegrowth_SwiftUI
+//
+//  Created by toui on 2023/10/07.
+//
+
+import UIKit
+import SwiftUI
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let hostingController = UIHostingController(rootView: HomeView())
+        addChild(hostingController)
+        view.addSubview(hostingController.view)
+        hostingController.view.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            hostingController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            hostingController.view.topAnchor.constraint(equalTo: view.topAnchor),
+            hostingController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            hostingController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
