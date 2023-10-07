6e205c9521aaef2c2c08da782f00aa1391d12dab
ghtoui <92294389+ghtoui@users.noreply.github.com>
2023-10-07 21:26:33 +0900

    initial commit

---
+++ vegegrowth_SwiftUI/Router.swift
@@ -0,0 +1,19 @@
+//
+//  Router.swift
+//  vegegrowth_SwiftUI
+//
+//  Created by toui on 2023/10/07.
+//
+
+import Foundation
+import UIKit
+
+enum ScreenRoute {
+    case home
+    case TakePicture
+    case
+}
+
+class Router {
+    
+}
