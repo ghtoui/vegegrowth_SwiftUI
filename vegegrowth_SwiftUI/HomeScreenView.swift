7206ff51d3feabe811613beaa0b1fcba8e6a4566
ghtoui <92294389+ghtoui@users.noreply.github.com>
2023-10-07 21:24:58 +0900

    tableからHomeに名称変更

---
+++ vegegrowth_SwiftUI/HomeScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  TableScreenView.swift
+//  vegegrowth_SwiftUI
+//
+//  Created by toui on 2023/10/07.
+//
+
+import SwiftUI
+
+struct HomeScreenView: View {
+    var body: some View {
+    }
+}
+
+// これがPreview
+struct TableScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        TableScreenView()
+    }
+}
