ffcc3897b8367f2625fe9a8095ae053e3805e6e5
ghtoui <92294389+ghtoui@users.noreply.github.com>
2023-10-07 16:40:32 +0900

    initial commit

---
+++ vegegrowth_SwiftUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vegegrowth_SwiftUI
+//
+//  Created by toui on 2023/10/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
